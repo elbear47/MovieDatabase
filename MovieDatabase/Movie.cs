1f27e140123986f91fe7eb991d3bac44b22f8574
Elber Funez <elberfunez@gmail.com>
2022-07-25 21:24:37 -0400

    first commit

---
+++ MovieDatabase/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace MovieDatabase
+{
+	public class Movie
+	{
+		private string title;
+		private string category;
+
+		public Movie(string title, string category)
+		{
+			Title = title;
+			Category = category;
+		}
+		public string Title { get; set; }
+		public string Category { get; set; }
+
+		public void getTitleBasedOnCategory(string cat)
+        {
+			Category = cat;
+
+            Console.WriteLine(cat);
+        }
+		
+	}
+	
+}
+
---
+++ MovieDatabase/Program.cs
@@ -0,0 +1,58 @@
+﻿using MovieDatabase;
+
+bool keepRunning = true;
+
+do
+{
+    // Create a list of 10 movies
+
+    List<Movie> moviesList = new List<Movie>();
+    moviesList.Add(new Movie("The God Father", "drama"));
+    moviesList.Add(new Movie("Cars", "animated"));
+    moviesList.Add(new Movie("The Departed", "drama"));
+    moviesList.Add(new Movie("The Lion King", "animated"));
+    moviesList.Add(new Movie("Finding Nemo", "animated"));
+    moviesList.Add(new Movie("The Mummy", "drama"));
+    moviesList.Add(new Movie("The Matrix", "scifi"));
+    moviesList.Add(new Movie("Top Gun", "drama"));
+    moviesList.Add(new Movie("White Chicks", "drama"));
+    moviesList.Add(new Movie("The Ring", "horror"));
+
+    //count of movies
+    int amtOfMovies = moviesList.Count();
+
+    Console.WriteLine("Welcome to the Movie List Application!");
+    Console.WriteLine("There are " + amtOfMovies + " movies in this collection");
+
+    Console.WriteLine("Please enter one of the following categories to display movie List \n 1) animated\n 2) drama\n 3) horror\n 4) scifi\n");
+
+    string genreSelected = Console.ReadLine().ToLower();
+    //print movie details for that category
+
+    List<Movie> relatedMovieList = moviesList.Where(x => x.Category == genreSelected).ToList();
+
+    foreach (Movie m in relatedMovieList)
+    {
+        Console.WriteLine(m.Title);
+    }
+    Console.WriteLine();
+
+
+
+    Console.WriteLine("Continue? (y/n)");
+    string response = Console.ReadLine().ToLower();
+
+    if (response == "y")
+
+    {
+        keepRunning = true;
+
+    }
+
+    else if (response == "n")
+    {
+        keepRunning = false;
+    }
+    else Console.WriteLine("Please enter a y or n");
+
+} while (keepRunning);
